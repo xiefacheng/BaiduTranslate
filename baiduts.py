7cf1f3f07da48a8aed6f8f8d1165ba85389b6a79
Nadou <xiefacheng@foxmail.com>
2021-08-30 17:02:30 +0800

    update

--- baiduts.py
+++ baiduts.py
@@ -7,13 +7,12 @@ def get_translate_info(word):
     # 通过F12查看传参方式为post
     dat = {'kw': word}
     resp = requests.post(url, data=dat)
-    # res =
-    # res_data =
     # 获取翻译后的字段
     translate_info = resp.json()['data'][0]['v']
     return translate_info
 
 
+# 调试功能是否正常
 if __name__ == '__main__':
     w1 = get_translate_info('dog')
     print(w1)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from BaiduTranslate.baiduts import get_translate_info
 from BaiduTranslate.excel_data import ExcelLoad
 
-
 exl = ExcelLoad(excel_path=r'C:\Users\Administrator\Documents\GitHub\BaiduTranslate\百度翻译.xlsx')
 sheet = exl.write_data('Sheet1')
 word = ''
+row = 2
 while True:
     word = input('输入查询单词：')
     if word == '!q':
@@ -12,4 +12,9 @@ while True:
     else:
         select_word = get_translate_info(word)
         print('查询的结果为：{}'.format(select_word))
-        
+        if sheet.cell(row, 2).value is not None:
+            row = row + 1
+        else:
+            sheet.cell(row, 2, word)
+            sheet.cell(row, 3, select_word)
+            exl.wb.save('测试.xlsx')
